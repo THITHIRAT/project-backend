db62a01b262cf08f0fa343d3db99a1d7864fb520
DESKTOP-BD4IU5S\Thithirat <thithirat.chsw@gmail.com>
2018-03-22 11:11:12 +0700

    create task/event

--- route/task.js
+++ route/task.js
@@ -32,11 +32,55 @@ router.post('/location', (req,res) => {
                 if(rows.length > 0) {
                     var id = rows[0]._id;
                     var complete = 0;
-                    connection.query('SELECT _id, user_id, type, notification, placename, latitude, longtitude, taskname, complete FROM reminder WHERE user_id = ? AND complete = ?', [id,complete], function(err, rows) {
+                    connection.query('SELECT _id, user_id, notification, placename, latitude, longtitude, taskname, complete FROM reminder WHERE user_id = ? AND complete = ? AND type= ?', [id,complete, "Location"], function(err, rows) {
                         if(err) {
                             res.send({
                                 status: 400,
                                 msg: 'there are some error with query select task location'
+                            });
+                        }else {
+                            res.send({
+                                status: 200,
+                                data: rows,
+                                msg: 'query success'
+                            });
+                        }
+                    });
+                }else {
+                    res.send({
+                        status: 404,
+                        msg: 'dont have token'
+                    });
+                }
+            }
+        });
+    }else {
+        res.send({
+            status: 403,
+            msg: "permission denied"
+        });
+    }
+});
+
+router.post('/event', (req,res) => {
+    var token = req.body.token;
+
+    if(token) {
+        connection.query('SELECT * FROM user WHERE token = ?', token, function(err, rows) {
+            if(err) {
+                res.send({
+                    status: 400,
+                    msg: 'there are some error with query select task event'
+                });
+            }else {
+                if(rows.length > 0) {
+                    var id = rows[0]._id;
+                    var complete = 0;
+                    connection.query('SELECT _id, user_id, allday, start_date, end_date, start_time, end_time, placename, latitude, longtitude, taskname, complete FROM reminder WHERE user_id = ? AND complete = ? AND type= ?', [id,complete, "Event"], function(err, rows) {
+                        if(err) {
+                            res.send({
+                                status: 400,
+                                msg: 'there are some error with query select task event'
                             });
                         }else {
                             res.send({
