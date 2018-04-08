da3642f911cadc8574799354a4c473a779a89a99
DESKTOP-BD4IU5S\Thithirat <thithirat.chsw@gmail.com>
2018-04-09 02:54:18 +0700

    edit bug and create case notification output

--- route/suggestreminder.js
+++ route/suggestreminder.js
@@ -36,7 +36,9 @@ function count_notification_1(start, num){
     var msec_start = start.getTime();
 
     var count_num = num - Math.round(num * 0.5);
-    if (count_num <= 29) {
+    if(count_num == 0) {
+        return "Before " + count_num + " Days";
+    }else if (count_num <= 29) {
         return "Before " + count_num + " Days";
     }else if(count_num > 29 && count_num <= 365) {
         var count_num_mth = Math.round(count_num/12);
@@ -50,7 +52,9 @@ function count_notification_2(start, num){
     var msec_start = start.getTime();
 
     var count_num = num - Math.round(num * 0.8);
-    if (count_num <= 29) {
+    if(count_num == 0) {
+        return  "Before " + count_num + " Days";
+    }else if (count_num <= 29) {
         return "Before " + count_num + " Days";
     }else if(count_num > 29 && count_num <= 365) {
         var count_num_mth = Math.round(count_num/12);
@@ -61,7 +65,7 @@ function count_notification_2(start, num){
 }
 
 router.post('/taskname', (req,res) => {
-    connection.query(`SELECT DISTINCT type FROM catalog`, function(err,rows) {
+    connection.query(`SELECT DISTINCT type FROM catalog ORDER BY type`, function(err,rows) {
         if(err) {
             res.send({
                 status: 400,
@@ -88,41 +92,57 @@ router.post('/tasknamenotification', (req,res) => {
                     msg: 'suggestreminder/tasknamenotification : there are some error with query select catalog'
                 });
             }else {
-                var num = rows[0].period_num;
-                var now = new Date();
-                var date = now.getDate();
-                var month = now.getMonth() + 1;
-                var year = now.getFullYear() + 543;
-                var startdate = date + "/" + month + "/" + year;
-                var end = count_enddate(now, num);
-                var e_date = end.getDate();
-                var e_month = end.getMonth() + 1;
-                var e_year = end.getFullYear() + 543;
-                var enddate = e_date + "/" + e_month + "/" + e_year;
+                if(rows.length > 0) {
+                    var num = rows[0].period_num;
+                    var now = new Date();
+                    var date = now.getDate();
+                    var month = now.getMonth() + 1;
+                    var year = now.getFullYear() + 543;
+                    var startdate = date + "/" + month + "/" + year;
+                    var end = count_enddate(now, num);
+                    var e_date = end.getDate();
+                    var e_month = end.getMonth() + 1;
+                    var e_year = end.getFullYear() + 543;
+                    var enddate = e_date + "/" + e_month + "/" + e_year;
+    
+                    var notification_1 = count_notification_1(now,num);
+                    var notification_2 = count_notification_2(now,num);
+    
+                    var output = {
+                        allday: "1",
+                        startdate: startdate,
+                        enddate: enddate,
+                    }
+                    var notification = {
+                        notification_1: notification_1,
+                        notification_2: notification_2
+                    }
 
-                var notification_1 = count_notification_1(now,num);
-                var notification_2 = count_notification_2(now,num);
-
-                var output = {
-                    allday: "1",
-                    startdate: startdate,
-                    enddate: enddate,
+                    if(notification.notification_1 == "zero" && notification.notification_2 == "zero") {
+                        res.send({
+                            status: 200,
+                            output: output,
+                            msg: 'suggestreminder/tasknamenotification : no before_after complete'
+                        });
+                    }else {
+                        res.send({
+                            status: 200,
+                            data: rows,
+                            output: output,
+                            notification: notification,
+                            msg: 'suggestreminder/tasknamenotification : add data complete'
+                        });
+                    }
+                }else {
+                    res.send({
+                        status: 200,
+                        msg: 'suggestreminder/tasknamenotification : complete'
+                    });
                 }
-                var notification = {
-                    notification_1: notification_1,
-                    notification_2: notification_2
-                }
-                res.send({
-                    status: 200,
-                    data: rows,
-                    output: output,
-                    notification: notification,
-                    msg: 'suggestreminder/tasknamenotification : complete'
-                });
             }
         });
     }else {
-        res.send({
+        res.send({  
             status: 403,
             msg: "suggestreminder/tasknamenotification : permission denied"
         });
@@ -133,7 +153,7 @@ router.post('/subtaskname', (req,res) => {
     var type = req.body.type;
 
     if(type) {
-        connection.query(`SELECT DISTINCT item FROM catalog`, function(err,rows){
+        connection.query(`SELECT DISTINCT item FROM catalog WHERE NOT item = ? AND type = ? ORDER BY item`, ["",type], function(err,rows){
             if(err) {
                 res.send({
                     status: 400,
@@ -141,9 +161,9 @@ router.post('/subtaskname', (req,res) => {
                 });
             }else {
                 res.send({
-                    status: 400,
+                    status: 200,
                     data: rows,
-                    msg: 'suggestreminder/subtaskname : there are some error with query select catalog'
+                    msg: 'suggestreminder/subtaskname : complete'
                 });
             }
         });
@@ -155,7 +175,7 @@ router.post('/subtaskname', (req,res) => {
     }
 });
 
-router.post('/subtasknamenotification', (req,res) => {
+router.post('/subtasknamenotification', (req,res) => {  
     var type = req.body.taskname;
     var item = req.body.subtaskname;
 
@@ -167,37 +187,54 @@ router.post('/subtasknamenotification', (req,res) => {
                     msg: 'suggestreminder/subtasknamenotification : there are some error with query select catalog'
                 });
             }else {
-                var num = rows[0].period_num;
-                var now = new Date();
-                var date = now.getDate();
-                var month = now.getMonth() + 1;
-                var year = now.getFullYear() + 543;
-                var startdate = date + "/" + month + "/" + year;
-                var end = count_enddate(now, num);
-                var e_date = end.getDate();
-                var e_month = end.getMonth() + 1;
-                var e_year = end.getFullYear() + 543;
-                var enddate = e_date + "/" + e_month + "/" + e_year;
+                if(rows.length > 0) {
+                    var num = rows[0].period_num;
+                    var now = new Date();
+                    var date = now.getDate();
+                    var month = now.getMonth() + 1;
+                    var year = now.getFullYear() + 543;
+                    var startdate = date + "/" + month + "/" + year;
+                    var end = count_enddate(now, num);
+                    var e_date = end.getDate();
+                    var e_month = end.getMonth() + 1;
+                    var e_year = end.getFullYear() + 543;
+                    var enddate = e_date + "/" + e_month + "/" + e_year;
+    
+                    var notification_1 = count_notification_1(now,num);
+                    var notification_2 = count_notification_2(now,num);
 
-                var notification_1 = count_notification_1(now,num);
-                var notification_2 = count_notification_2(now,num);
+                    var output = {
+                        allday: "1",
+                        startdate: startdate,
+                        enddate: enddate,
+                    }
+                    var notification = {
+                        notification_1: notification_1,
+                        notification_2: notification_2
+                    }
 
-                var output = {
-                    allday: "1",
-                    startdate: startdate,
-                    enddate: enddate,
+                    if(notification.notification_1 == "" && notification.notification_2 == "") {
+                        res.send({
+                            status: 200,
+                            output: output,
+                            msg: 'suggestreminder/subtasknamenotification : no before_after complete'
+                        });
+                    }else {
+                        res.send({
+                            status: 200,
+                            data: rows,
+                            output: output,
+                            notification: notification,
+                            msg: 'suggestreminder/subtasknamenotification : add data complete'
+                        });
+                    }
+                }else {
+                    console.log("select row = 0");
+                    res.send({
+                        status: 200,
+                        msg: 'suggestreminder/subtasknamenotification : complete'
+                    });
                 }
-                var notification = {
-                    notification_1: notification_1,
-                    notification_2: notification_2
-                }
-                res.send({
-                    status: 200,
-                    data: rows,
-                    output: output,
-                    notification: notification,
-                    msg: 'suggestreminder/subtasknamenotification : complete'
-                });
             }
         });
     }else {
